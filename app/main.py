f139d5062ac841f2afea284198122a5ab525fe6f
Harshad Bajirao Khatale <khataleharshad26@gmail.com>
2026-03-19 21:26:58 +0530

    done

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 import os
 from fastapi import FastAPI
-import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
-def root():
-    return {"message": "API Running 🚀"}
+def home():
+    return {"message": "API running 🚀"}
 
 if __name__ == "__main__":
+    import uvicorn
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run("app.main:app", host="0.0.0.0", port=port)
