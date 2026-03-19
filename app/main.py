7d777296e9ac54f8581cd3778bd15c771d198455
Harshad Bajirao Khatale <khataleharshad26@gmail.com>
2026-03-19 23:50:04 +0530

    done

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 import os
 import uvicorn
 from fastapi import FastAPI
-
 from app.routes.auth_routes import router as auth_router
 
 app = FastAPI()
+
+app.include_router(auth_router)
 
 @app.get("/")
 def home():
